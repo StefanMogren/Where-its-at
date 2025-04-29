6ab5f4e1718da07b9ab75e1f31eda7522e59dadf
StefanMogren <stefan_mogren@hotmail.com>
2025-04-29 17:59:08 +0200

    Created useFetchEvents.js

---
+++ src/hooks/useFetchEvents.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useFetchEvents = () => {
+	const [events, setEvents] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(null);
+
+	useEffect(() => {
+		setIsLoading(true);
+		axios
+			.get('https://santosnr6.github.io/Data/events.json')
+			.then((response) => setEvents(response.data))
+			.catch((error) => setIsError(error))
+			.finally(() => setIsLoading(false));
+	}, []);
+
+	return { events, isLoading, isError };
+};
