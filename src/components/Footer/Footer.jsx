efcb77368e9127bda1f93c8b3b09f40972649513
StefanMogren <stefan_mogren@hotmail.com>
2025-04-29 15:24:24 +0200

    Added dependencies for @fortawesome/free-solid-svg-icons, added faCircle to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
+// library.add(faCircle);
+// const test = window.FontAwesome.icon({ prefix: 'fas', iconName: 'circle' });
 
 // Ska innehålla de tre punkterna för swiper
 function Footer() {
-	return <footer>Footer med tre punkter</footer>;
+	return (
+		<footer className='footer'>
+			<FontAwesomeIcon
+				icon={faCircle}
+				size='2xs'
+				style={{ color: '#ffffff' }}
+			/>
+			<FontAwesomeIcon
+				icon={faCircle}
+				size='2xs'
+				style={{ color: '#FFFFFF4D' }}
+			/>
+			<FontAwesomeIcon
+				icon={faCircle}
+				size='2xs'
+				style={{ color: '#FFFFFF4D' }}
+			/>
+		</footer>
+	);
 }
 
 export default Footer;
