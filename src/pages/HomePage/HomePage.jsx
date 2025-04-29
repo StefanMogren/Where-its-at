023cb62e5c5ea83af4cd016788e2d56506c65312
StefanMogren <stefan_mogren@hotmail.com>
2025-04-29 14:42:28 +0200

    Added content to HomePage, imported fonts Sansita, Fira Sans, and Libre Barcode 128

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,18 @@
 import './homePage.css';
+import logo from '../../assets/logo.png';
 
 function HomePage() {
-	return <main>HomePage</main>;
+	return (
+		<main className='home-page'>
+			<img
+				className='home-page__img'
+				src={logo}
+				alt="Logga Where it's at"
+			/>
+			<h1 className='home-page__title'>Where it's @</h1>
+			<h2 className='home-page__subtitle'>Ticketing made easy</h2>
+		</main>
+	);
 }
 
 export default HomePage;
