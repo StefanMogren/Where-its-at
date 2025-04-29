6f8ee44cda49066a4528b2e2d8abdfb9cb405f52
StefanMogren <stefan_mogren@hotmail.com>
2025-04-29 17:49:39 +0200

    Created EventItem, added call in EventPage

---
+++ src/components/EventItem/EventItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EventItem() {
+	return <li>EventItem is here</li>;
+}
+
+export default EventItem;
--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,9 +1,19 @@
+import EventItem from '../../components/EventItem/EventItem';
 import './eventsPage.css';
 
 function EventsPage() {
 	return (
-		<main>
+		<main className='events-page'>
 			<h1>Events</h1>
+			<input
+				className='events-page__search'
+				type='search'
+				name='eventSearch'
+				id='eventSearch'
+			/>
+			<ul className='events-page__event-container'>
+				<EventItem />
+			</ul>
 		</main>
 	);
 }
