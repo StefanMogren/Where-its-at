cd942ff43bc48b3980d9859ee441f17ffc6ac0d3
StefanMogren <stefan_mogren@hotmail.com>
2025-04-30 09:30:51 +0200

    Added call for useFetchEvents in ContentPage

--- src/pages/ContentPage/ContentPage.jsx
+++ src/pages/ContentPage/ContentPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import HomePage from '../HomePage/HomePage';
 import EventsPage from '../EventsPage/EventsPage';
 import TicketsPage from '../TicketsPage/TicketsPage';
@@ -6,16 +5,27 @@ import Footer from '../../components/Footer/Footer';
 
 import useEventStore from '../../stores/useEventStore';
 import { useFetchEvents } from '../../hooks/useFetchEvents';
+import { useEffect } from 'react';
 
 // Ska använda Swiper för att hoppa mellan HomePage, EventsPage och TicketsPage
+
 function ContentPage() {
-	const { events, isLoading, isError } = useFetchEvents();
+	const { events: eventsAPI, isLoading, isError } = useFetchEvents();
+
+	const { events, setEvents } = useEventStore();
+
+	useEffect(() => {
+		if (events.length === 0 && eventsAPI.length > 0) {
+			setEvents(eventsAPI);
+		}
+	}, [eventsAPI]);
 
 	console.log(events);
+
 	return (
 		<>
 			{/* <HomePage /> */}
-			<EventsPage />
+			{events && <EventsPage events={events} />}
 			<Footer />
 		</>
 	);
