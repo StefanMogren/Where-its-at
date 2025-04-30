23fee4b62e395405de85290eef4931a695cbd4db
StefanMogren <stefan_mogren@hotmail.com>
2025-04-30 13:33:54 +0200

    Added styling to .events-page__search, .events-page__icon, and input

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,4 +1,6 @@
 import EventItem from '../../components/EventItem/EventItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './eventsPage.css';
 import useEventStore from '../../stores/useEventStore';
 
@@ -8,12 +10,16 @@ function EventsPage() {
 	return (
 		<main className='events-page'>
 			<h1 className='events-page__title'>Events</h1>
-			<input
-				className='events-page__search'
-				type='search'
-				name='eventSearch'
-				id='eventSearch'
-			/>
+			<label className='events-page__search' htmlFor='eventSearch'>
+				<FontAwesomeIcon
+					className='events-page__icon'
+					icon={faMagnifyingGlass}
+					style={{ color: '#FFFFFF33' }}
+					size='xl'
+				/>
+				<input type='search' name='eventSearch' id='eventSearch' />
+			</label>
+
 			<ul className='events-page__event-container'>
 				{events.map((event) => (
 					<EventItem event={event} key={event.id} />
