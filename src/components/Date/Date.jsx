d9fe4ae773237e423afb6e848badccfcd265a428
StefanMogren <stefan_mogren@hotmail.com>
2025-04-30 10:56:16 +0200

    Created Date.jsx, added call inside EventItem

---
+++ src/components/Date/Date.jsx
@@ -0,0 +1,73 @@
+function Date({ when }) {
+	// Delar upp dagen och månaden i två värden
+	let dateArray = when.split(' ');
+
+	const [date, month] = dateArray;
+
+	// Omvandlar datumet till ett värde som dateTime-attributet i <time>-elementet godtar.
+	const fullDate = () => {
+		const yearNumber = '2025-';
+
+		// Gör om månaden till ett strängnummer
+		let monthNumber;
+		switch (month) {
+			case 'Januari':
+				monthNumber = '01-';
+				break;
+
+			case 'Februari':
+				monthNumber = '02-';
+				break;
+
+			case 'Mars':
+				monthNumber = '03-';
+				break;
+
+			case 'April':
+				monthNumber = '04-';
+				break;
+
+			case 'Maj':
+				monthNumber = '05-';
+				break;
+
+			case 'Juni':
+				monthNumber = '06-';
+				break;
+
+			case 'Juli':
+				monthNumber = '07-';
+				break;
+
+			case 'Augusti':
+				monthNumber = '08-';
+				break;
+
+			case 'September':
+				monthNumber = '09-';
+				break;
+
+			case 'Oktober':
+				monthNumber = '10-';
+				break;
+
+			case 'November':
+				monthNumber = '11-';
+				break;
+
+			case 'December':
+				monthNumber = '12-';
+				break;
+		}
+
+		return `${yearNumber + monthNumber + date.padStart(2, '0')}`;
+	};
+
+	return (
+		<time dateTime={fullDate()}>
+			<span>{date}</span> <span>{month.slice(0, 3)}</span>
+		</time>
+	);
+}
+
+export default Date;
--- src/components/EventItem/EventItem.jsx
+++ src/components/EventItem/EventItem.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
 import './eventItem.css';
 import TimeFromTo from '../TimeFromTo/TimeFromTo';
+import Date from '../Date/Date';
 
 function EventItem({ event }) {
-	const { id, name, price, when, where } = event;
+	const {
+		id,
+		name,
+		price,
+		when: { date, from, to },
+		where,
+	} = event;
+
 	return (
 		<li className='event-item'>
-			<time>{when.date}</time>
+			<Date when={date} />
 			<section>
 				<div>
 					<h2>{name}</h2>
 					<p>{where}</p>
 					<p>
-						<TimeFromTo from={when.from} to={when.to} />
+						<TimeFromTo from={from} to={to} />
 					</p>
 				</div>
 				<p>{price} sek</p>
