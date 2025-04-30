eeed7638098200511710b77ad3d409e8ed8cf8a4
StefanMogren <stefan_mogren@hotmail.com>
2025-04-30 18:36:03 +0200

    Added initial design and style to AddEventPage

--- src/pages/AddEventPage/AddEventPage.jsx
+++ src/pages/AddEventPage/AddEventPage.jsx
@@ -1,6 +1,7 @@
 import './addEventPage.css';
 import useEventStore from '../../stores/useEventStore';
 import { useParams } from 'react-router-dom';
+import TimeFromTo from '../../components/TimeFromTo/TimeFromTo';
 
 function AddEventPage() {
 	const { events, setEvents } = useEventStore();
@@ -10,18 +11,29 @@ function AddEventPage() {
 	const currentEvent = events.find((event) => event.id === id);
 	console.log(events);
 
-	console.log(currentEvent);
+	const {
+		name,
+		price,
+		when: { date, from, to },
+		where,
+	} = currentEvent;
 
 	return (
-		<main>
-			<h1>Event</h1>
-			<p>You are about to score some tickets to</p>
-			<section>
-				<h2>titel</h2>
-				<p>dag kl tid</p>
-				<p>plats</p>
-			</section>
-		</main>
+		<section className='wrapper'>
+			<main className='add-event-page'>
+				<h1 className='add-event-page__title'>Event</h1>
+				<p className='add-event-page__subtitle'>
+					You are about to score some tickets to
+				</p>
+				<section className='add-event-page__info'>
+					<h2 className='add-event-page__event-title'>{name}</h2>
+					<p className='add-event-page__date'>
+						{date} kl {<TimeFromTo from={from} to={to} />}
+					</p>
+					<p className='add-event-page__location'>@ {where}</p>
+				</section>
+			</main>
+		</section>
 	);
 }
 
