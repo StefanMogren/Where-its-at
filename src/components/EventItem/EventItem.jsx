c31f1c2cccc54227a80e946d76ebcf53ae3a07eb
StefanMogren <stefan_mogren@hotmail.com>
2025-04-30 14:08:47 +0200

    Added Link to EventItem, moved CSS class from li to Link

--- src/components/EventItem/EventItem.jsx
+++ src/components/EventItem/EventItem.jsx
@@ -1,6 +1,7 @@
 import './eventItem.css';
 import TimeFromTo from '../TimeFromTo/TimeFromTo';
 import Date from '../Date/Date';
+import { Link } from 'react-router-dom';
 
 function EventItem({ event }) {
 	const {
@@ -12,22 +13,24 @@ function EventItem({ event }) {
 	} = event;
 
 	return (
-		<li className='event-item'>
-			<div className='event-item__date'>
-				<Date when={date} />
-			</div>
-			<section className='event-item__info'>
-				<h2 className='event-item__title'>{name}</h2>
-				<section className='event-item__details'>
-					<div className='event-item__inner-details'>
-						<p className='event-item__location'>{where}</p>
-						<p className='event-item__time'>
-							<TimeFromTo from={from} to={to} />
-						</p>
-					</div>
-					<p className='event-item__price'>{price} sek</p>
+		<li>
+			<Link className='event-item' to={`add-event/${id}`}>
+				<div className='event-item__date'>
+					<Date when={date} />
+				</div>
+				<section className='event-item__info'>
+					<h2 className='event-item__title'>{name}</h2>
+					<section className='event-item__details'>
+						<div className='event-item__inner-details'>
+							<p className='event-item__location'>{where}</p>
+							<p className='event-item__time'>
+								<TimeFromTo from={from} to={to} />
+							</p>
+						</div>
+						<p className='event-item__price'>{price} sek</p>
+					</section>
 				</section>
-			</section>
+			</Link>
 		</li>
 	);
 }
