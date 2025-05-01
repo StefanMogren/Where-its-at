6a0807f1bf6ccae6a17d7bff8147ba746702910a
StefanMogren <stefan_mogren@hotmail.com>
2025-05-01 15:30:20 +0200

    Added link to /order page in Basket.jsx

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -3,11 +3,14 @@ import { faTicket } from '@fortawesome/free-solid-svg-icons';
 import './basket.css';
 import useEventStore from '../../stores/useEventStore';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Basket() {
 	const { events } = useEventStore();
 	const [totalAmount, setTotalAmount] = useState(0);
 
+	// Kollar igenom alla event.amount för att räkna totalt antal
+	// Uppdaterar antalet som visas ovanpå biljett-ikonen
 	useEffect(() => {
 		setTotalAmount(0);
 		events.forEach((event) => {
@@ -15,15 +18,19 @@ function Basket() {
 		});
 	}, [events]);
 
+	// Navigerar användaren till "/order" sidan
+	const navigate = useNavigate();
+	const goToOrderPage = () => {
+		const path = '/order';
+		navigate(path);
+	};
+
 	return (
-		<button
-			className='basket'
-			onClick={() => console.log('Basket ticket is clicked!')}>
-			<FontAwesomeIcon
-				icon={faTicket}
-				style={{ color: `var(--pink)` }}
-				// size='2xl'
-			/>
+		<button className='basket' onClick={goToOrderPage}>
+			{/* --- Ikonen --- */}
+			<FontAwesomeIcon icon={faTicket} style={{ color: `var(--pink)` }} />
+
+			{/* --- Antalet ovanpå ikonen --- */}
 			{totalAmount > 0 ? (
 				<span className='basket__count'>{totalAmount}</span>
 			) : (
