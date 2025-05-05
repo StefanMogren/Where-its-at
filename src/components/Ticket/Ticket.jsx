3bad3f0604e4cf74ee31961a91faeb456d180331
StefanMogren <stefan_mogren@hotmail.com>
2025-05-05 15:42:53 +0200

    Moved feature to check for adjacent seats from Ticket.jsx to TicketsPage.jsx

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,66 +1,48 @@
 import Date from '../Date/Date';
 import './ticket.css';
 
-function Ticket({ event }) {
-	let adjacentSeats = [];
-	for (let i = 0; i < event.amount; i++) {
-		adjacentSeats.push(i);
-	}
-	console.log(adjacentSeats);
-
+function Ticket({ event, seat }) {
 	return (
-		<>
-			{adjacentSeats.map((seat) => {
-				return (
-					<article
-						className='ticket'
-						key={event.section + (event.seat + seat)}>
-						<section className='ticket__what'>
-							<h2>WHAT</h2>
-							<p className='ticket__title'>{event.name}</p>
-						</section>
+		<article className='ticket'>
+			<section className='ticket__what'>
+				<h2>WHAT</h2>
+				<p className='ticket__title'>{event.name}</p>
+			</section>
 
-						<section className='ticket__where'>
-							<h2>WHERE</h2>
-							<p>{event.where}</p>
-						</section>
+			<section className='ticket__where'>
+				<h2>WHERE</h2>
+				<p>{event.where}</p>
+			</section>
 
-						<section className='ticket__date-time'>
-							<section className='ticket__when'>
-								<h2>WHEN</h2>
-								<p>
-									<Date
-										when={event.when.date}
-										shorten={false}
-									/>
-								</p>
-							</section>
-							<section className='ticket__from'>
-								<h2>FROM</h2>
-								<p>{event.when.from}</p>
-							</section>
-							<section className='ticket__to'>
-								<h2>TO</h2>
-								<p>{event.when.to}</p>
-							</section>
-						</section>
+			<section className='ticket__date-time'>
+				<section className='ticket__when'>
+					<h2>WHEN</h2>
+					<p>
+						<Date when={event.when.date} shorten={true} />
+					</p>
+				</section>
+				<section className='ticket__from'>
+					<h2>FROM</h2>
+					<p>{event.when.from}</p>
+				</section>
+				<section className='ticket__to'>
+					<h2>TO</h2>
+					<p>{event.when.to}</p>
+				</section>
+			</section>
 
-						<section className='ticket__info'>
-							<h2>INFO</h2>
-							<p>
-								Section {event.section} - seat{' '}
-								{event.seat + seat}{' '}
-							</p>
-						</section>
+			<section className='ticket__info'>
+				<h2>INFO</h2>
+				<p>
+					Section {event.section} - seat {seat}{' '}
+				</p>
+			</section>
 
-						<section className='ticket__identifier'>
-							<h2 className='ticket__barcode'>#A2ED7</h2>
-							<p className='ticket__id'>#A2ED7</p>
-						</section>
-					</article>
-				);
-			})}
-		</>
+			<section className='ticket__identifier'>
+				<h2 className='ticket__barcode'>#A2ED7</h2>
+				<p className='ticket__id'>#A2ED7</p>
+			</section>
+		</article>
 	);
 }
 
--- src/pages/TicketsPage/TicketsPage.jsx
+++ src/pages/TicketsPage/TicketsPage.jsx
@@ -3,12 +3,27 @@ import './ticketsPage.css';
 import useTicketStore from '../../stores/useTicketStore';
 
 function TicketsPage() {
-	const { tickets, addTicket } = useTicketStore();
+	const { tickets } = useTicketStore();
+	console.log('Tickets are');
+
+	console.log(tickets);
+
 	return (
 		<section className='wrapper gradient'>
 			<main className='tickets-page'>
 				{tickets.map((event) => {
-					return <Ticket event={event} />;
+					const adjacentSeats = [];
+					for (let i = 0; i < event.amount; i++) {
+						const seat = event.seat + i;
+						adjacentSeats.push(
+							<Ticket
+								event={event}
+								seat={seat}
+								key={event.section + seat}
+							/>
+						);
+					}
+					return adjacentSeats;
 				})}
 			</main>
 		</section>
