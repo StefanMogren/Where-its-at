593c112247dd815f3b46ef0c7478cffb2cc55d71
StefanMogren <stefan_mogren@hotmail.com>
2025-05-05 13:36:48 +0200

    Added more CSS to Ticket.jsx

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,39 +1,65 @@
+import Date from '../Date/Date';
 import './ticket.css';
 
 function Ticket({ event }) {
+	const letterIndex = Math.floor(Math.random() * 26) + 65;
+	const randomSection = String.fromCharCode(letterIndex);
+	const randomSeat = Math.floor(Math.random() * 500) + 1;
+
+	let adjacentSeats = [];
+	for (let i = 0; i < event.amount; i++) {
+		adjacentSeats.push(i);
+	}
+	console.log(adjacentSeats);
+
 	return (
-		<article className='ticket'>
-			<section className='ticket__what'>
-				<h2>WHAT</h2>
-				<p className='ticket__title'>{event.name}</p>
-			</section>
-			<section className='ticket__where'>
-				<h2>WHERE</h2>
-				<p>{event.where}</p>
-			</section>
-			<section className='ticket__date-time'>
-				<section>
-					<h2>WHEN</h2>
-					<p>{event.when.date}</p>
-				</section>
-				<section>
-					<h2>FROM</h2>
-					<p>{event.when.from}</p>
-				</section>
-				<section>
-					<h2>TO</h2>
-					<p>{event.when.to}</p>
-				</section>
-			</section>
-			<section className='ticket__info'>
-				<h2>INFO</h2>
-				<p>Section and seat, and info</p>
-			</section>
-			<section className='ticket__identifier'>
-				<h2 className='ticket__barcode'>#A2ED7</h2>
-				<p className='ticket__id'>#A2ED7</p>
-			</section>
-		</article>
+		<>
+			{adjacentSeats.map((seat) => {
+				return (
+					<article className='ticket'>
+						<section className='ticket__what'>
+							<h2>WHAT</h2>
+							<p className='ticket__title'>{event.name}</p>
+						</section>
+
+						<section className='ticket__where'>
+							<h2>WHERE</h2>
+							<p>{event.where}</p>
+						</section>
+
+						<section className='ticket__date-time'>
+							<section className='ticket__when'>
+								<h2>WHEN</h2>
+								<p>
+									<Date when={event.when.date} />
+								</p>
+							</section>
+							<section className='ticket__from'>
+								<h2>FROM</h2>
+								<p>{event.when.from}</p>
+							</section>
+							<section className='ticket__to'>
+								<h2>TO</h2>
+								<p>{event.when.to}</p>
+							</section>
+						</section>
+
+						<section className='ticket__info'>
+							<h2>INFO</h2>
+							<p>
+								Section {randomSection} - seat{' '}
+								{randomSeat + seat}{' '}
+							</p>
+						</section>
+
+						<section className='ticket__identifier'>
+							<h2 className='ticket__barcode'>#A2ED7</h2>
+							<p className='ticket__id'>#A2ED7</p>
+						</section>
+					</article>
+				);
+			})}
+		</>
 	);
 }
 
--- src/pages/TicketsPage/TicketsPage.jsx
+++ src/pages/TicketsPage/TicketsPage.jsx
@@ -5,7 +5,7 @@ import useTicketStore from '../../stores/useTicketStore';
 function TicketsPage() {
 	const { tickets, addTicket } = useTicketStore();
 	return (
-		<section className='wrapper'>
+		<section className='wrapper gradient'>
 			<main className='tickets-page'>
 				{tickets.map((event) => {
 					return <Ticket event={event} />;
