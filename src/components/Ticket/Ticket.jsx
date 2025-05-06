8e62d9a9365aec25ebe218611c6c8c8e643943a9
StefanMogren <stefan_mogren@hotmail.com>
2025-05-06 10:11:31 +0200

    Added comments

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -19,33 +19,41 @@ function Ticket({ event, seat }) {
 
 	return (
 		<article className='ticket'>
+			{/* --- Eventets namn --- */}
 			<section className='ticket__what'>
 				<h2>WHAT</h2>
 				<p className='ticket__title'>{event.name}</p>
 			</section>
 
+			{/* --- Platsen --- */}
 			<section className='ticket__where'>
 				<h2>WHERE</h2>
 				<p>{event.where}</p>
 			</section>
 
 			<section className='ticket__date-time'>
+				{/* --- Datumet --- */}
 				<section className='ticket__when'>
 					<h2>WHEN</h2>
 					<p>
 						<Date when={event.when.date} shorten={true} />
 					</p>
 				</section>
+
+				{/* --- Tiden från --- */}
 				<section className='ticket__from'>
 					<h2>FROM</h2>
 					<p>{event.when.from}</p>
 				</section>
+
+				{/* --- Tiden till --- */}
 				<section className='ticket__to'>
 					<h2>TO</h2>
 					<p>{event.when.to}</p>
 				</section>
 			</section>
 
+			{/* --- Sittplatsen --- */}
 			<section className='ticket__info'>
 				<h2>INFO</h2>
 				<p>
@@ -53,6 +61,7 @@ function Ticket({ event, seat }) {
 				</p>
 			</section>
 
+			{/* --- Streckkod och biljettID --- */}
 			<section className='ticket__identifier'>
 				<h2 className='ticket__barcode'>{ticketID}</h2>
 				<p className='ticket__id'>{ticketID}</p>
