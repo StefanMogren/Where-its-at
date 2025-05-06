b4aa8a94e02bd2b0398abcc07965eb1a8c19d753
StefanMogren <stefan_mogren@hotmail.com>
2025-05-06 10:55:31 +0200

    Destructured prop ticket in Ticket.jsx

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -2,18 +2,19 @@ import Date from '../Date/Date';
 import './ticket.css';
 
 function Ticket({ ticket }) {
+	const { name, where, when, section, seat, ticketID } = ticket;
 	return (
 		<article className='ticket'>
 			{/* --- Eventets namn --- */}
 			<section className='ticket__what'>
 				<h2>WHAT</h2>
-				<p className='ticket__title'>{ticket.name}</p>
+				<p className='ticket__title'>{name}</p>
 			</section>
 
 			{/* --- Platsen --- */}
 			<section className='ticket__where'>
 				<h2>WHERE</h2>
-				<p>{ticket.where}</p>
+				<p>{where}</p>
 			</section>
 
 			<section className='ticket__date-time'>
@@ -21,20 +22,20 @@ function Ticket({ ticket }) {
 				<section className='ticket__when'>
 					<h2>WHEN</h2>
 					<p>
-						<Date when={ticket.when.date} shorten={true} />
+						<Date when={when.date} shorten={true} />
 					</p>
 				</section>
 
 				{/* --- Tiden från --- */}
 				<section className='ticket__from'>
 					<h2>FROM</h2>
-					<p>{ticket.when.from}</p>
+					<p>{when.from}</p>
 				</section>
 
 				{/* --- Tiden till --- */}
 				<section className='ticket__to'>
 					<h2>TO</h2>
-					<p>{ticket.when.to}</p>
+					<p>{when.to}</p>
 				</section>
 			</section>
 
@@ -42,14 +43,14 @@ function Ticket({ ticket }) {
 			<section className='ticket__info'>
 				<h2>INFO</h2>
 				<p>
-					Section {ticket.section} - seat {ticket.seat}
+					Section {section} - seat {seat}
 				</p>
 			</section>
 
 			{/* --- Streckkod och biljettID --- */}
 			<section className='ticket__identifier'>
-				<h2 className='ticket__barcode'>{ticket.ticketID}</h2>
-				<p className='ticket__id'>{ticket.ticketID}</p>
+				<h2 className='ticket__barcode'>{ticketID}</h2>
+				<p className='ticket__id'>{ticketID}</p>
 			</section>
 		</article>
 	);
