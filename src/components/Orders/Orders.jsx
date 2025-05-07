0d0ba6b814467a4e477f2798821a1757a6e9cd61
StefanMogren <stefan_mogren@hotmail.com>
2025-05-07 13:59:18 +0200

    Created Orders component, moved majority of content from OrderPage.jsx into Orders.jsx, imported Orders.jsx into OrderPage.jsx, added ternary in OrderPage.jsx to check if there are any orders available.

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,98 @@
+import useEventStore from '../../stores/useEventStore';
+import useTicketStore from '../../stores/useTicketStore';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import { useEffect, useState } from 'react';
+import TicketCounter from '../../components/TicketCounter/TicketCounter';
+import Date from '../../components/Date/Date';
+import Time from '../Time/Time';
+import generateTicketID from '../../utils/generateTicketID';
+
+function Orders() {
+	const {
+		events,
+		increaseEventAmount,
+		decreaseEventAmount,
+		emptyAllEventAmount,
+	} = useEventStore();
+
+	const { tickets, addTicket } = useTicketStore();
+
+	const [totalCost, setTotalCost] = useState(0);
+
+	useEffect(() => {
+		let totalCost = 0;
+		events.forEach((event) => {
+			totalCost += event.amount * event.price;
+		});
+		setTotalCost(totalCost);
+	}, [events]);
+
+	const navigate = useNavigate();
+	const purchaseTickets = () => {
+		events.forEach((event) => {
+			if (event.amount > 0) {
+				const letterIndex = Math.floor(Math.random() * 26) + 65;
+				const randomSection = String.fromCharCode(letterIndex);
+
+				const randomSeat = Math.floor(Math.random() * 300) + 1;
+				for (let i = 0; i < event.amount; i++) {
+					const seat = randomSeat + i;
+
+					addTicket(event, randomSection, seat, generateTicketID());
+				}
+			}
+		});
+
+		//Töm useEventStore
+		emptyAllEventAmount();
+
+		// Gå till /tickets sidan
+		const path = '/tickets';
+		navigate(path);
+	};
+
+	console.log(tickets);
+
+	return (
+		<>
+			{events.map((event) => {
+				if (event.amount > 0) {
+					return (
+						// --- TicketCounter, wrappar eventnamnet och datumet/tiden ---
+						<TicketCounter
+							key={event.id}
+							amount={event.amount}
+							increaseAmount={() => increaseEventAmount(event.id)}
+							decreaseAmount={() =>
+								decreaseEventAmount(event.id)
+							}>
+							{/* --- Eventnamnet --- */}
+							<h2 className='order-page__event-title'>
+								{event.name}
+							</h2>
+
+							{/* --- Datumet och tiden --- */}
+							<p className='order-page__event-info'>
+								<Date when={event.when.date} shorten={false} />
+								{' kl '}
+								<Time time={event.when.from} />
+								{' - '}
+								<Time time={event.when.to} />
+							</p>
+						</TicketCounter>
+					);
+				}
+			})}
+
+			{/* --- Totala kostnaden, skicka-knappen --- */}
+			<section className='order-page__summary'>
+				Totalt värde på order
+				<span className='order-page__total-cost'>{totalCost} sek</span>
+				<Button onClick={purchaseTickets}>Skicka order</Button>
+			</section>
+		</>
+	);
+}
+
+export default Orders;
--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,64 +1,11 @@
 import './orderPage.css';
 import useEventStore from '../../stores/useEventStore';
-import useTicketStore from '../../stores/useTicketStore';
-import TicketCounter from '../../components/TicketCounter/TicketCounter';
-import Time from '../../components/Time/Time';
-import Button from '../../components/Button/Button';
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Date from '../../components/Date/Date';
-import generateTicketID from '../../utils/generateTicketID';
+
 import NavigateBack from '../../components/NavigateBack/NavigateBack';
+import Orders from '../../components/Orders/Orders';
 
 function OrderPage() {
-	const {
-		events,
-		increaseEventAmount,
-		decreaseEventAmount,
-		emptyAllEventAmount,
-	} = useEventStore();
-	const { tickets, addTicket } = useTicketStore();
-
-	// Lokal useState för den totala kostnaden.
-	const [totalCost, setTotalCost] = useState(0);
-
-	// Varje gång events från useEventStore uppdateras (när amount ändras) så sätts den totala kostnaden
-	useEffect(() => {
-		let totalCost = 0;
-		events.forEach((event) => {
-			totalCost += event.amount * event.price;
-		});
-		setTotalCost(totalCost);
-	}, [events]);
-
-	// Funktion för "Skicka order"-knappen
-	// Skickar in alla events med amount > 0 till useTicketStore.
-	// Ger varje ticket en slumpmässig sektion och säte att utgå från
-	// Sätter alla useEventStore amount till 0.
-	const navigate = useNavigate();
-	const purchaseTickets = () => {
-		events.forEach((event) => {
-			if (event.amount > 0) {
-				const letterIndex = Math.floor(Math.random() * 26) + 65;
-				const randomSection = String.fromCharCode(letterIndex);
-
-				const randomSeat = Math.floor(Math.random() * 300) + 1;
-				for (let i = 0; i < event.amount; i++) {
-					const seat = randomSeat + i;
-
-					addTicket(event, randomSection, seat, generateTicketID());
-				}
-			}
-		});
-
-		//Töm useEventStore
-		emptyAllEventAmount();
-
-		// Gå till /tickets sidan
-		const path = '/tickets';
-		navigate(path);
-	};
-	console.log(tickets);
+	const { events } = useEventStore();
 
 	return (
 		<section className='wrapper'>
@@ -69,48 +16,13 @@ function OrderPage() {
 				</header>
 
 				{/* --- Varje event med amount > 0 får en TicketCounter */}
-				{events.map((event) => {
-					if (event.amount > 0) {
-						return (
-							// --- TicketCounter ---
-							<TicketCounter
-								key={event.id}
-								amount={event.amount}
-								increaseAmount={() =>
-									increaseEventAmount(event.id)
-								}
-								decreaseAmount={() =>
-									decreaseEventAmount(event.id)
-								}>
-								{/* --- Eventnamnet, wrappat av TicketCounter --- */}
-								<h2 className='order-page__event-title'>
-									{event.name}
-								</h2>
-
-								{/* --- Datumet och tiden, wrappat av TicketCounter */}
-								<p className='order-page__event-info'>
-									<Date
-										when={event.when.date}
-										shorten={false}
-									/>
-									{' kl '}
-									<Time time={event.when.from} />
-									{' - '}
-									<Time time={event.when.to} />
-								</p>
-							</TicketCounter>
-						);
-					}
-				})}
-
-				{/* --- Totala kostnaden, skicka-knappen --- */}
-				<section className='order-page__summary'>
-					Totalt värde på order
-					<span className='order-page__total-cost'>
-						{totalCost} sek
-					</span>
-					<Button onClick={purchaseTickets}>Skicka order</Button>
-				</section>
+				{events.find((event) => event.amount > 0) ? (
+					<Orders />
+				) : (
+					<h2 className='order-page__no-orders'>
+						Du har ännu inga biljetter att köpa
+					</h2>
+				)}
 			</main>
 		</section>
 	);
