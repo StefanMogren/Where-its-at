74eeab74a6d6f6b8e2e8527caa2106e1f655ea6a
StefanMogren <stefan_mogren@hotmail.com>
2025-05-07 14:38:52 +0200

    Removed unused content in Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,6 @@
-// import { Outlet } from 'react-router-dom';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AddEventPage from '../pages/AddEventPage/AddEventPage';
 import OrderPage from '../pages/OrderPage/OrderPage';
-// import ContentPage from '../pages/ContentPage/ContentPage';
 import SwiperWrapper from './SwiperWrapper';
 
 export const Router = () => {
@@ -26,34 +24,3 @@ export const Router = () => {
 		</BrowserRouter>
 	);
 };
-
-/* function Layout() {
-	return (
-		<section className='wrapper'>
-			<Outlet />
-		</section>
-	);
-} */
-
-/* export const Router = createBrowserRouter([
-	{
-		element: <Layout />,
-		errorElement: <h1>Insert 404 page here</h1>,
-		children: [
-			{
-				path: '/',
-				// Är den som innehåller swiper för de tre huvudsidorna
-				element: <ContentPage />,
-			},
-			{
-				path: '/add-event/:id',
-				element: <AddEventPage />,
-			},
-			{
-				path: '/order',
-				element: <OrderPage />,
-			},
-		],
-	},
-]);
- */
