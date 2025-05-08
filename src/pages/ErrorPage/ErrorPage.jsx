c6f20c0cb48c275239c70de9932eab7e15c2541b
StefanMogren <stefan_mogren@hotmail.com>
2025-05-08 11:31:22 +0200

    Created ErrorPage.jsx, added call in Router.jsx for 404 pages.

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './errorPage.css';
+import { Link } from 'react-router-dom';
+
+function ErrorPage() {
+	return (
+		<section className='wrapper'>
+			<main className='error-page'>
+				<h1 className='error-page__title'>404</h1>
+				<h2 className='error-page__subtitle'>
+					Hoppsan! Den här sidan verkar inte existera
+				</h2>
+				<Link className='error-page__link' to={'/events'}>
+					Klicka här för att gå tillbaka till eventssidan
+				</Link>
+			</main>
+		</section>
+	);
+}
+
+export default ErrorPage;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AddEventPage from '../pages/AddEventPage/AddEventPage';
 import OrderPage from '../pages/OrderPage/OrderPage';
 import SwiperWrapper from './SwiperWrapper';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
 export const Router = () => {
 	return (
@@ -20,6 +21,7 @@ export const Router = () => {
 				<Route path='/order' element={<OrderPage />} />
 
 				<Route path='/' element={<Navigate to='/home' />} />
+				<Route path='*' element={<ErrorPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
